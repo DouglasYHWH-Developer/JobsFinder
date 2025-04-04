1d7da2ea3fe5ac3374bb0f2ca5c85981713945b7
Douglas <dougurasu104@hotmail.com>
2025-04-04 12:10:25 -0300

    atualização

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize({
   dialect: "sqlite", // dialect é qual banco que no caso seria sqlite(nome do banco)
-  storage: "./db/app.db", // storage = armazenar. Agende coloca um caminho(opcional) seguido do nome do banco que no caso seria o app.db
+  storage: "./db/app.db", // storage = armazenar. Agente coloca um caminho(opcional) seguido do nome do banco que no caso seria o app.db
 });
 
 module.exports = sequelize;
