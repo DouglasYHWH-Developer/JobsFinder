669b10d7041e8e306c9364127620659d655a84a1
Douglas <dougurasu104@hotmail.com>
2025-04-22 10:12:12 -0300

    adição do pacote path em app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express    = require("express");
 const exphbs     = require("express-handlebars")
 const app        = express();
+const path       = require("path");
 const db         = require("./db/connection");
 const bodyParser = require('body-parser');
 
