81caccbf35d9396f68cc3687e23402e7bd20b4a2
Douglas <dougurasu104@hotmail.com>
2025-05-26 11:26:55 -0300

    continuação

---
+++ app.js
@@ -0,0 +1,52 @@
+const express    = require("express");
+const exphbs     = require("express-handlebars")
+const app        = express();
+const path       = require("path");
+const db         = require("./db/connection");
+const bodyParser = require('body-parser');
+const Job       = require('./models/Job')
+
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`O express está rodando na porta ${PORT}`);
+});
+
+//body parser
+app.use(bodyParser.urlencoded({extended: false}));
+
+//handle bars
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars'); // estamos dizendo que a nossa 'view engina' vai utilizar o framework/biblioteca handlebars
+
+//static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// db connection
+db.authenticate()
+  .then(() => {
+    console.log("conectou ao banco com sucesso");
+  })
+  .catch((err) => {
+    console.log("Ocorreu um erro ao conectar", err);
+  });
+
+// routes
+app.get("/", (req, res) => {
+
+  let search = req.body.job
+
+  Job.findAll({order: [
+    ['createdAt', 'DESC']
+  ]})
+  .then(jobs => {
+    res.render('index',{
+      jobs
+    });
+  });
+});
+
+// jobs routes
+app.use('/jobs', require('./routes/jobs'));
