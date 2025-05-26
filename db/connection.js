adf289de25c0cbc58c2b3776f5bc3a70468a6642
Douglas <dougurasu104@hotmail.com>
2025-05-26 11:26:24 -0300

    continuação

--- db/connection.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize({
-  dialect: "sqlite", // Diz qual tipo de banco está sendo usado.No seu caso, é o SQLite, que salva os dados em um arquivo local.
-  storage: "./db/app.db", // Esse é o caminho onde o arquivo do banco será salvo. 
-  // Você está dizendo: "guarde o banco em uma pasta db com o nome app.db".
-});
-
-module.exports = sequelize;
